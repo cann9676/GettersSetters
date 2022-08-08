8e6b02108262bd2a799fc892be0c99f982d72e45
cann9676 <80699111+cann9676@users.noreply.github.com>
2022-08-08 02:25:48 -0400

    Initial Commit

---
+++ GettersSetters/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  GettersSetters
+//
+//  Created by Chelsea Hannah on 8/8/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
